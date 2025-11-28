cd4f99b55258d9f4750ac743ef3ce1f38821dee4
Emmanuelraj <immanuel.raj18@gmail.com>
2025-11-28 18:28:02 +0530

    QnA-transcript

---
+++ week-12-QnATranscript/src/huggingfaceQnA.ts
@@ -0,0 +1,29 @@
+import { pipeline } from '@xenova/transformers';
+
+async  function main(){
+const generator = await pipeline('summarization', 'Xenova/distilbart-cnn-6-6');
+const text = 'The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, ' +
+  'and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. ' +
+  'During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest ' +
+  'man-made structure in the world, a title it held for 41 years until the Chrysler Building in New ' +
+  'York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to ' +
+  'the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the ' +
+  'Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second ' +
+  'tallest free-standing structure in France after the Millau Viaduct.';
+const output = await generator(text, {
+  max_new_tokens: 500,
+} as any);
+
+console.log(output);
+// [{ summary_text: ' The Eiffel Tower is about the same height as an 81-storey building and the tallest structure in Paris. It is the second tallest free-standing structure in France after the Millau Viaduct.' }]
+
+const answerer = await pipeline('question-answering', 'Xenova/distilbert-base-uncased-distilled-squad');
+const question = 'Who was Jim Henson?';
+const context = 'My name is Raj i works as a software developer';
+const outputDone = await answerer(question, context);
+
+console.log(outputDone)
+
+}
+
+main();
